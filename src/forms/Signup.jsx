ea48d6d389a60913cf90212655ac92b6050ae768
Victor Edet Coleman <141645829+colemanvictor25@users.noreply.github.com>
2024-04-26 12:16:08 -1100

    update

--- src/forms/Signup.jsx
+++ src/forms/Signup.jsx
@@ -9,11 +9,11 @@ export default function Signup() {
 <form  className="max-w-sm mx-auto mt-12 rounded-lg shadow-md p-10" action="#" method="POST">
   <div className="mb-5">
     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-    <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+    <input style={{background:"#343434",border:"none",outline:"#303030"}} type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
   </div>
   <div className="mb-5">
     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-    <input type="password" id="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+    <input style={{background:"#343434",border:"none",outline:"#303030"}} type="password" id="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
   </div>
   <div className="mb-5">
     <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Repeat password</label>
@@ -21,7 +21,7 @@ export default function Signup() {
   </div>
   <div className="flex items-start mb-5">
     <div className="flex items-center h-5">
-      <input id="terms" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+      <input style={{background:"#343434",border:"none",outline:"#303030"}} id="terms" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
     </div>
     <label  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a></label>
   </div>
--- src/layouts/Team.jsx
+++ src/layouts/Team.jsx
@@ -1,39 +1,14 @@
 export default function Team() {
     return (
       <>
-        <div className="p-20">
-          <div className="text-center mb-16">
-            <p className="mt-4 text-sm leading-7 text-gray-500 font-regular">
-              THE TEAM
-            </p>
-            <h3 className="text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight text-gray-900">
-              Meet Our <span className="text-indigo-600">Team</span>
-            </h3>
-          </div>
-  
-          <div className="sm:grid grid-cols-2 md:grid-cols-4 col-gap-10 mx-auto">
-            <div className="text-center">
-              <a href="#">
-                <img
-                  className="mb-3 rounded-xl mx-auto h-32 w-32"
-                  src="https://scontent-cdg4-2.xx.fbcdn.net/v/t39.30808-6/425004215_346944451661613_6972460022672526955_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFrqYXV91n8y48tcTnVEriYixTXn_UQORCLFNef9RA5EGN77P84FfOyPh5m55iYU4fPyNS7le7X0AfT_WQd4erZ&_nc_ohc=B5PKxc6dH6kAb5DhAT-&_nc_ht=scontent-cdg4-2.xx&cb_e2o_trans=q&oh=00_AfBuEgw1h0BEHouZWk95XnBzmodVKKoIFOV9yCag4dohJQ&oe=662F364B"
-                  alt="Team member"
-                />
-              </a>
-              <a
-                href="#"
-                className="hover:text-indigo-500 text-gray-900 font-semibold"
-              >
-                John Doe
-              </a>
-              <p className="text-gray-500 uppercase text-sm">Web Developer</p>
-              <div className="my-4 flex justify-center items-center">
-                {/* Remove unused anchor tags */}
-              </div>
-            </div>
-            {/* Add more team members here if needed */}
-          </div>
-        </div>
+        <div className="text-center mt-10">
+  <img
+    src="https://tecdn.b-cdn.net/img/new/avatars/5.webp"
+    className="mx-auto mb-4 w-32 rounded-lg"
+    alt="Avatar" />
+  <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
+  <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
+</div>
       </>
     );
   }
